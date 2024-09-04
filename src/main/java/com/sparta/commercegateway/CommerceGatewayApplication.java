5ba57096b817ce45379f86f7f71883df5eae3909
kimchaerin <puclpu18@gmail.com>
2024-09-04 16:56:47 +0900

    🎉 init commit

---
+++ src/main/java/com/sparta/commercegateway/CommerceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.commercegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommerceGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sparta/commercegateway/CommerceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.commercegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
